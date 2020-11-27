d1a688df778c9f7e4a72dd6c70dbb08d34bd7012
rmnldwg <48687784+rmnldwg@users.noreply.github.com>
2020-11-27 16:19:25 +0100

    updated setup.py

--- setup.py
+++ setup.py
@@ -4,19 +4,19 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="lymph_rmnldwg", # Replace with your own username
-    version="0.1.1",
-    author="Roman Ludwig",
-    author_email="roman.ludwig@usz.ch",
-    description="Package for statistical modelling of lymphatic metastatic spread.",
-    long_description=long_description,
-    long_description_content_type="text/markdown",
-    # url="https://github.com/pypa/sampleproject",
-    packages=setuptools.find_packages(),
-    classifiers=[
+    name = "lymph", # Replace with your own username
+    version = "0.1.1",
+    author = "Roman Ludwig",
+    author_email = "roman.ludwig@usz.ch",
+    description = "Package for statistical modelling of lymphatic metastatic spread.",
+    long_description = long_description,
+    long_description_content_type = "text/markdown",
+    url = "https://github.com/rmnldwg/lymph",
+    packages = setuptools.find_packages(),
+    classifiers = [
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    python_requires='>=3.6',
+    python_requires = '>=3.6',
 )
