859b3b44b9f9ec0ff1f485985305e453dbdd9575
rmnldwg <48687784+rmnldwg@users.noreply.github.com>
2021-04-09 16:16:07 +0200

    replaced setup.py & improved package metadata

--- lymph/__init__.py
+++ lymph/__init__.py
@@ -1,3 +1,12 @@
+__description__ = "Package for statistical modelling of lymphatic metastatic spread."
+__author__ = "Roman Ludwig"
+__email__ = "roman.ludwig@usz.ch"
+__uri__ = "https://github.com/rmnldwg/lymph"
+
+from ._version import get_versions
+__version__ = get_versions()['version']
+del get_versions
+
 import numpy as np
 import scipy as sp 
 import scipy.stats
@@ -5,6 +14,11 @@ import scipy.stats
 from .node import *
 from .edge import *
 from .system import *
-from ._version import get_versions
-__version__ = get_versions()['version']
-del get_versions
+
+__all__ = [
+    "Node",
+    "Edge",
+    "System",
+    "BilateralSystem",
+    "toStr",
+]
