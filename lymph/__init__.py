38534e6b57e115f09b0670997aeaf8e178edb811
rmnldwg <48687784+rmnldwg@users.noreply.github.com>
2022-03-22 15:48:52 +0100

    now use setuptools_scm & better requirements

--- lymph/__init__.py
+++ lymph/__init__.py
@@ -1,18 +1,17 @@
-"""This package contains code to model the spread of microscopic metastases
+"""
+This package contains code to model the spread of microscopic metastases
 through a system of lymph node levels (LNLs), using either a Bayesian network
-or a hidden Markov model."""
+or a hidden Markov model.
+"""
 
+from ._version import version
+__version__ = version
 __description__ = "Package for statistical modelling of lymphatic metastatic spread."
 __author__ = "Roman Ludwig"
 __email__ = "roman.ludwig@usz.ch"
 __uri__ = "https://github.com/rmnldwg/lymph"
 
 # nopycln: file
-
-from ._version import get_versions
-
-__version__ = get_versions()['version']
-del get_versions
 
 from .bilateral import Bilateral, BilateralSystem
 from .edge import Edge
