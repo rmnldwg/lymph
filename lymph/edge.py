256d30c8e2029176984b9fe513f82c3478d66777
Roman Ludwig <48687784+rmnldwg@users.noreply.github.com>
2023-06-20 17:27:31 +0200

    change!: re-implement the `Edge` class
    This now matches the new node classes. It strictly follows the principle
    of "Separation of Concerns" and is more pythonic & modular than before.
    
    BREAKING CHANGE: Does not work with any previous versions.
    Related: #40, #41, #44, #45

--- lymph/edge.py
+++ lymph/edge.py
@@ -1,99 +1,146 @@
+"""
+This module implements the edges of the graph representation of the lymphatic system.
+"""
 from __future__ import annotations
+from typing import Union
 
-from .node import Node
+from lymph.node import AbstractNode, Tumor, LymphNodeLevel
 
 
-class Edge(object):
-    """Minimalistic class for the connections between lymph node levels (LNLs)
-    represented by the :class:`Node` class. It only holds its start and end
-    node, as well as the transition probability.
+class Edge:
     """
-    def __init__(self, start: Node, end: Node, base_t: float = 0., microscopic_parameter: float = 1.):
+    This class represents an arc in the graph representation of the lymphatic system.
+    """
+    def __init__(
+        self,
+        start: Union[Tumor, LymphNodeLevel],
+        end: LymphNodeLevel,
+        spread_prob: float = 0.,
+        micro_mod: float = 1.,
+    ):
+        """Create a new edge between two nodes.
+
+        The `start` node must be a `Tumor` or a `LymphNodeLevel`, and the `end` node
+        must be a `LymphNodeLevel`.
+
+        The `spread_prob` parameter is the probability of a tumor or involved LNL to
+        spread to the next LNL. The `micro_mod` parameter is a modifier for the spread
+        probability in case of only a microscopic node involvement.
         """
-        Args:
-            start: Parent node
-            end: Child node
-            t: Transition probability in case start-Node has state 1 (microscopic
-                involvement).
-        """
-        if not isinstance(start, Node):
+        self.start = start
+        self.end = end
+
+        self.micro_mod = micro_mod
+        self.spread_prob = spread_prob
+
+
+    def __str__(self):
+        """Print basic info."""
+        return f"{self.start}-->{self.end}"
+
+
+    @property
+    def start(self) -> Union[Tumor, LymphNodeLevel]:
+        """Return the start (parent) node of the edge."""
+        return self._start
+
+    @start.setter
+    def start(self, new_start: Union[Tumor, LymphNodeLevel]) -> None:
+        """Set the start (parent) node of the edge."""
+        if not issubclass(new_start, AbstractNode):
             raise TypeError("Start must be instance of Node!")
-        if not isinstance(end, Node):
+
+        self._start = new_start
+        self.start.out.append(self)
+
+
+    @property
+    def end(self) -> LymphNodeLevel:
+        """Return the end (child) node of the edge."""
+        return self._end
+
+    @end.setter
+    def end(self, new_end: LymphNodeLevel) -> None:
+        """Set the end (child) node of the edge."""
+        if not isinstance(new_end, LymphNodeLevel):
             raise TypeError("End must be instance of Node!")
 
-
-        self.start = start
-        self.start.out.append(self)
-        self.end = end
+        self._end = new_end
         self.end.inc.append(self)
-        self.microscopic_parameter = float(microscopic_parameter)
-        self.base_t = base_t
-        
-        
-    def __str__(self):
-        """Print basic info"""
-        return f"{self.start}-{100 * self.t:.1f}%->{self.end}"
-    
-    
-    @property
-    def microscopic_parameter(self):
-        """Return the microscopic spread parameter that is multiplied with the
-        base transition probabilities in case of a microscopic spread
-        """
-        return self._microscopic_parameter
-    
-    @microscopic_parameter.setter
-    def microscopic_parameter(self, new_microscopic_parameter: float):
-        """Set the microscopic spread parameter for LNLs
-        """
-        if new_microscopic_parameter <= 1. and new_microscopic_parameter >= 0.:
-            self._microscopic_parameter = new_microscopic_parameter
-            if hasattr(self, "_t"):
-                del self._t
-        else:
-            raise ValueError("microscopic spread parameter must be between 0 and 1")
-        
-        
+
+
     @property
     def is_growth(self) -> bool:
         """Check if this edge represents a node's growth."""
         return self.start == self.end
-    
-    
-    @property
-    def base_t(self):
-        """Holds the base transition probability for transition to the next state.
-        For growth edges the base transition probability is the growth probability.
-        For edges between LNLs the base transition probability holds the transition
-        probability for a macroscopic parent node/a involved node in the binary model.
-        The base_t is multiplied with the macroscopic parameter if needed.      
-        """
-        return self._base_t
 
-    @base_t.setter
-    def base_t(self, new_base_t: float):
-        """ Sets the base_t parameter and the transition _t parameter.
-        This method checks the edge type and parent node to compute the final 
-        transition probability.
-        """
-        self._base_t = new_base_t 
-        if self.start.state == 0:
-            self._t = 0.
-        elif self.is_growth:
-            self._t = self._base_t
-        elif new_base_t <= 1. and new_base_t >= 0.:
-            self._t = self._base_t * self.microscopic_parameter if self.start.state == 1 and self.start.allowed_states == 3 else self._base_t
-        else:
-            raise ValueError("Transmission probability must be between 0 and 1")
-        
 
     @property
-    def t(self):
-        """Returns the transition probability applied by an edge after considering
-        whether it is a growth/microscopic/macroscopic edge.
+    def is_tumor_spread(self) -> bool:
+        """Check if this edge represents spread from a tumor to an LNL."""
+        return isinstance(self.start, Tumor)
+
+
+    @property
+    def micro_mod(self):
+        """Return the spread modifier for LNLs with microscopic involvement."""
+        return self._micro_mod
+
+    @micro_mod.setter
+    def micro_mod(self, new_micro_mod: float) -> None:
+        """Set the spread modifier for LNLs with microscopic involvement."""
+        new_micro_mod = float(new_micro_mod)
+
+        if not (0. <= new_micro_mod <= 1.):
+            raise ValueError("Microscopic spread modifier must be between 0 and 1!")
+
+        self._micro_mod = new_micro_mod
+
+
+    @property
+    def spread_prob(self):
+        """Return the spread probability of the edge.
+
+        This is the probability of a tumor or involved LNL to spread to the next LNL.
+        Its value may be modified by the `micro_mod` parameter, when an LNL has only
+        microscopic involvement.
         """
-        try:
-            return self._t
-        except AttributeError:
-            self.base_t = self._base_t
-            return self._t
+        return self._spread_prob
+
+    @spread_prob.setter
+    def spread_prob(self, new_spread_prob: float) -> None:
+        """Set the spread probability of the edge."""
+        new_spread_prob = float(new_spread_prob)
+
+        if not (0. <= new_spread_prob <= 1.):
+            raise ValueError("Spread probability must be between 0 and 1!")
+
+        self._spread_prob = new_spread_prob
+
+
+    def comp_bayes_net_prob(self, log: bool = False) -> float:
+        """Compute the conditional probability of this edge's child node's state.
+
+        This function dynamically computes the conditional probability that the child
+        node is in its state, given the parent node's state and the parameters of the
+        edge.
+        """
+
+        # Implementing this is not a priority, but it would be nice to have.
+        raise NotImplementedError("Not implemented yet!")
+
+
+    def comp_spread_prob(self, new_state: int, log: bool = False) -> float:
+        """Compute the probability of spread per time-step in the hidden Markov model.
+
+        This function dynamically computes the probability of spread per time-step that
+        the child node will transition to `new_state`, given its current state, that
+        of its parent node, and the parameters of the edge.
+        """
+
+        # I was thinking that here you could make use of the properties `is_growth`
+        # and `is_tumor_spread` to compute the value. It also needs to take into
+        # account the `start.state` and `end.state`.
+        # Note that you do not need to cover the case where `new_state < end.state`,
+        # because that is already covered in the implementation of the `AbstractNode`.
+        raise NotImplementedError("TODO: Implement this!")
