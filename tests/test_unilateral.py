97d736121862b7ca12ca0f9f244ae88d0fb58f56
rmnldwg <48687784+rmnldwg@users.noreply.github.com>
2023-07-19 18:20:01 +0200

    tests(uni): trans mat tests (not passing yet)

--- tests/test_unilateral.py
+++ tests/test_unilateral.py
@@ -22,6 +22,11 @@ class BinaryFixtureMixin:
         }
         self.model = Unilateral(graph=self.graph)
 
+    def create_random_params(self, seed: int = 42) -> Dict[str, float]:
+        """Create random parameters for the model."""
+        rng = np.random.default_rng(seed)
+        return {name: rng.random() for name in self.model.edge_params.keys()}
+
 
 class InitBinaryTestCase(BinaryFixtureMixin, unittest.TestCase):
     """Test the initialization of a binary model."""
@@ -76,11 +81,6 @@ class InitBinaryTestCase(BinaryFixtureMixin, unittest.TestCase):
 class BinaryParameterAssignmentTestCase(BinaryFixtureMixin, unittest.TestCase):
     """Test the assignment of parameters in a binary model."""
 
-    def create_random_params(self, seed: int = 42) -> Dict[str, float]:
-        """Create random parameters for the model."""
-        rng = np.random.default_rng(seed)
-        return {name: rng.random() for name in self.model.edge_params.keys()}
-
     def test_edge_params_assignment_via_lookup(self):
         """Make sure the spread parameters are assigned correctly."""
         params_to_set = self.create_random_params(seed=42)
@@ -100,17 +100,43 @@ class BinaryParameterAssignmentTestCase(BinaryFixtureMixin, unittest.TestCase):
         with self.assertRaises(TypeError):
             self.model.edge_params["spread_T_to_I"] = 0.5
 
-    def _test_transition_matrix_deletion(self):
+    def test_transition_matrix_deletion(self):
         """Check if the transition matrix gets deleted when a parameter is set.
 
         NOTE: This test is disabled because apparently, the `model` instance is
         changed during the test and the `_transition_matrix` attribute is deleted on
         the wrong instance. I have no clue why, but generally, the method works.
         """
-        self.model._gen_transition_matrix()
+        _ = self.model.transition_matrix
         self.assertTrue(hasattr(self.model, "_transition_matrix"))
         self.model.edge_params["spread_T_to_I"].set(0.5)
         self.assertFalse(hasattr(self.model, "_transition_matrix"))
+
+
+class BinaryTransitionMatrixTestCase(BinaryFixtureMixin, unittest.TestCase):
+    """Test the generation of the transition matrix in a binary model."""
+
+    def setUp(self):
+        """Initialize a simple binary model."""
+        self.graph = {
+            ("tumor", "T"): ["II", "III"],
+            ("lnl", "II"): ["III"],
+            ("lnl", "III"): [],
+        }
+        self.model = Unilateral(graph=self.graph)
+
+        params_to_set = self.create_random_params(seed=42)
+        self.model.assign_parameters(**params_to_set)
+
+    def test_transition_matrix_shape(self):
+        """Make sure the transition matrix has the correct shape."""
+        self.assertEqual(self.model.transition_matrix.shape, (4, 4))
+
+    def test_transition_matrix_row_sums(self):
+        """Make sure the rows of the transition matrix sum to one."""
+        row_sums = np.sum(self.model.transition_matrix, axis=1)
+        self.assertTrue(np.allclose(row_sums, 1.0))
+
 
 if __name__ == "__main__":
     unittest.main()
