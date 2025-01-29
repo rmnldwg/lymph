09725e7bc422f44aa2fb96df68c7c355469adac3
Roman Ludwig <48687784+rmnldwg@users.noreply.github.com>
2025-01-29 09:56:46 +0100

    test: add basic tests for named params mixin

---
+++ tests/params_test.py
@@ -0,0 +1,123 @@
+"""Test if setting params via the named subset works correctly."""
+
+import pytest
+import numpy as np
+
+from .fixtures import (
+    get_graph,
+    _create_random_frozen_dist,
+    _create_random_parametric_dist,
+    RNG
+)
+
+from lymph import models
+
+
+@pytest.fixture()
+def binary_unilateral_model() -> models.Unilateral:
+    """Return a binary unilateral model."""
+    model = models.Unilateral(graph_dict=get_graph(size="small"))
+    model.set_distribution(
+        t_stage="early",
+        distribution=_create_random_frozen_dist(max_time=model.max_time),
+    )
+    model.set_distribution(
+        t_stage="late",
+        distribution=_create_random_parametric_dist(max_time=model.max_time),
+    )
+    return model
+
+
+def test_set_named_params_default_behaviour(
+    binary_unilateral_model: models.Unilateral,
+) -> None:
+    """Ensure `set_named_params` works as `set_params` when no `named_params` set."""
+    params = binary_unilateral_model.get_params(as_dict=True)
+    new_params = {param: RNG.uniform() for param in params.keys()}
+    binary_unilateral_model.set_named_params(**new_params)
+    assert new_params == binary_unilateral_model.get_params(as_dict=True)
+
+
+def test_named_params_setter(
+    binary_unilateral_model: models.Unilateral,
+) -> None:
+    """Check that setting `named_params` works correctly."""
+    with pytest.raises(ValueError):
+        binary_unilateral_model.named_params = ["some_param"]
+
+    with pytest.raises(ValueError):
+        binary_unilateral_model.named_params = ["invalid identifier!"]
+
+    with pytest.raises(ValueError):
+        binary_unilateral_model.named_params = 123
+
+    params = binary_unilateral_model.get_params(as_dict=True).keys()
+    params_subset = [param for param in params if RNG.uniform() > 0.5]
+    binary_unilateral_model.named_params = params_subset
+
+    for stored, subset in zip(binary_unilateral_model.named_params, params_subset):
+        assert stored == subset
+
+
+def test_set_named_params_named_easy_subset(
+    binary_unilateral_model: models.Unilateral,
+) -> None:
+    """Ensure `set_named_params` works correctly with an easy subset.
+
+    An "easy subset" is a literal subset of the params.
+    """
+    params = binary_unilateral_model.get_params(as_dict=True)
+    new_params = {param: RNG.uniform() for param in params.keys()}
+    params_subset = {k: RNG.uniform() for k in params if RNG.uniform() > 0.5}
+
+    binary_unilateral_model.set_params(**new_params)
+    binary_unilateral_model.named_params = params_subset.keys()
+    binary_unilateral_model.set_named_params(**params_subset)
+
+    for param, new_val, subset_val in zip(
+        params.keys(),
+        new_params.values(),
+        params_subset.values(),
+    ):
+        stored_params = binary_unilateral_model.get_params(as_dict=True)
+        if param in params_subset:
+            assert subset_val == stored_params[param]
+        else:
+            assert new_val == stored_params[param]
+
+    assert set(params_subset.keys()) == set(binary_unilateral_model.named_params)
+
+
+def test_set_named_params_named_hard_subset(
+    binary_unilateral_model: models.Unilateral,
+) -> None:
+    """Ensure `set_named_params` works correctly with a hard subset.
+
+    A "hard subset" is a subset that includes "global params". I.e., `spread` would
+    not be a literal subset, because those are nemad something like `TtoII_spread`. But
+    the `set_params()` method does accept it and will set all spread params with the
+    provided value. It should be possible to set the `named_params` to such names and
+    then set them with the `set_named_params()` method.
+    """
+    params = binary_unilateral_model.get_params(as_dict=True)
+    new_params = {param: RNG.uniform() for param in params.keys()}
+    first_lnl = list(binary_unilateral_model.graph.lnls.keys())[0]
+    first_lnl_param = f"Tto{first_lnl}_spread"
+    params_subset = {k: RNG.uniform() for k in ["spread", first_lnl_param]}
+
+    binary_unilateral_model.set_params(**new_params)
+    binary_unilateral_model.named_params = params_subset.keys()
+    binary_unilateral_model.set_named_params(**params_subset)
+
+    stored_params = binary_unilateral_model.get_params(as_dict=True)
+    for param, new_val, stored_param in zip(
+        params.keys(),
+        new_params.values(),
+        stored_params.values(),
+    ):
+        if param == first_lnl_param:
+            assert params_subset[first_lnl_param] == stored_param
+        elif "spread" in param:
+            assert params_subset["spread"] == stored_param
+        else:
+            assert new_val == stored_param
